7952e142ee0fe9369ea407a8ad5acd9388b12531
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-05 00:12:16 +0530

    overlay

--- pages/index.js
+++ pages/index.js
@@ -32,8 +32,19 @@ const Home = () => {
           </motion.div>
         </div>
       </div>
-      <div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat
+        w-full h-full absolute mix-blend-color-dodge translate-z-0">
 
+        </div>
+        {/* Particles */}
+        <div>
+          Particles
+        </div>
+        {/* Avater */}
+        <div>
+          <Avatar />
+        </div>
       </div>
     </div>
   );
