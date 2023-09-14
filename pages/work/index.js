9bc1ce42acbbc127291fce3a6261c24ea6787c84
abhiCS7A60 <88727740+abhinv98@users.noreply.github.com>
2023-09-14 22:31:33 +0530

    final commit

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,6 +1,51 @@
-import ServiceSlider from '../../components/ServiceSlider';
-import Bulb from '../../components/Bulb';
-import Circles from '../../components/Circles';
+import WorkSlider from "../../components/WorkSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
 
-import { motion } from 'framer-motion';
-import {fadeIn} from '../../variants';
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
+const Work = () => {
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
+      <Circles />
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row gap-x-8">
+          <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
+            <motion.h2
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-[100px]"
+            >
+              My Works <span className="text-accent">.</span>
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("right", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0 "
+            >
+              I have selected about 4 projects to display here, that show best what my passions are. If you want to see more of my projects, 
+              please visit my Github Link.
+            </motion.p>
+          </div>
+          <motion.div
+            variants={fadeIn("right", 0.2)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            <WorkSlider />
+          </motion.div>
+        </div>
+      </div>
+      <Bulb />
+    </div>
+  );
+};
+
+export default Work;
